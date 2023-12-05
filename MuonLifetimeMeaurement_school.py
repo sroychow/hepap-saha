d55ba7669eb85439cfc5a2f6375498c3b5c24e8c
sbaradia21 <swetabaradai13@gmail.com>
2023-12-05 02:13:02 +0100

    Updating the files for students

--- MuonLifetimeMeaurement_school.py
+++ MuonLifetimeMeaurement_school.py
@@ -7,31 +7,21 @@ from iminuit import Minuit
 # Part1: Create exponential random numbers for a given value of tau and plot it ;
 # Exponential pdf = 1/tau*e^(-t/tau)
 
-##transforming U(0,1) to exponential random number t
+##transformi U(0,1) to exponential random number t
 def generate_time(n1,tau):
-    ran = ROOT.TRandom3(0)
     root_time_measurements = []
-    for _ in range(n1):
-        r1 = ran.Uniform(0.0, 1.0)
-        r = -np.log(r1)/(1/tau)
-        root_time_measurements.append(r)
+    #TODO:generate exponential random numbers->store in root_time_measurements
     return root_time_measurements
 
 ##generating exponential random number t using numpy
 def generate_time_numpy(n1,tau):
-    numpy_time_measurements = np.random.exponential(scale=tau, size=n1)
+    #TODO:generate random number using numpy->store in numpy_time_measurements
     return numpy_time_measurements
 
 ##filling tree with the time measuremnets
 def fill_tree_with_time(outfile, time_measurements):
     tree = ROOT.TTree("time_measurements", "Time Measurement Data")
-    x = np.zeros(1, dtype=float)
-    tree.Branch("x", x, "x/D")
-
-    for r in time_measurements:
-        x[0] = r
-        tree.Fill()
-
+    #TODO: Fill the tree branch with time measurements from outfile
     tree.Write()
 
 ##plot the time measurements
@@ -63,15 +53,7 @@ def fit_time_measurements(outfile):
     tree = outfile.Get("time_measurements")
 
     def fcn(tau):
-        time = [entry.x for entry in tree]
-        lnL = 0.0
-        for x in time:
-            pdf = (np.exp(-x/tau))/tau
-            if pdf > 0.0:
-                lnL += np.log(pdf)
-            else:
-                print("pdf is negative!!!")
-        f = -2.0 * lnL
+    #TODO: Read the random numbers from outfile and generate 2NLL (f)
         return f
     minuit = Minuit(fcn, tau=2)
     minuit.limits["tau"] = (2, 10)
@@ -109,20 +91,14 @@ def plot_time_measurement_fit(outfile, tau_expected):
 tau_expected = fit_time_measurements(outfile)
 plot_time_measurement_fit(outfile, tau_expected)
 
-# Part 3: Do n2 such experiments and plot the fitted taus ; Please have a look at the mean of the gaussian (is it close to the tau value taken?)
+# Part 3: Do n2 such experiments and plot the fitted taus ; Please have a look at the mean of the gaussian
 # The Central Limit Theorem:
 # For samples of size  30 or more, the sample mean is approximately normally distributed, with mean mu  and standard deviation = tau/sqrt(n); where tau = std of the sample, n  is the sample size. 
 
 n2=1000 #total number of experiments
 tau_expected_samples = []
 print("Fitting the samples")
-for _ in range(n2):
-    time_measurements_sample = np.array(generate_time(n1,tau))
-    #time_measurements_samples = np.array(generate_time_numpy(n1,tau))# to use numpy
-    outfileTemp = ROOT.TFile("DecayTimeMeasurementsSample.root", "recreate")
-    fill_tree_with_time(outfileTemp, time_measurements_sample)
-    tau_expected_sample  = fit_time_measurements(outfileTemp)
-    tau_expected_samples.append(tau_expected_sample)
+#TODO: Do n2 such experiments: for each experiment fit the n1 random number's  distribution->calculate the expected value of tau->store the expected values in tau_expected_sample
 
 def plot_tau_expected(tau_expected_samples):
     plt.figure(figsize=(12, 6))
@@ -138,9 +114,7 @@ plot_tau_expected(tau_expected_samples)
 # Part 4: From the distribution of expected tau, calculate the std and match with the expected (sigma/sqrt(n)) one from CLT
 
 def check_error(tau_expected_samples,n1,n2,tau):
-    #sample_mean_std1 = np.std(samples_mean)
-    sum_squared_diff = np.sum((tau_expected_samples - np.mean(tau_expected_samples))**2)
-    tau_expected_std = np.sqrt(sum_squared_diff/(n2-1))
+    #TODO:Find the Error on tau expected from tau_expected_samples -> tau_expected_std
     time_measurements_std = tau/(np.sqrt(n1))
     print(f"Error on the tau expected: {tau_expected_std:.4f}")
     print(f"Expected std : {time_measurements_std:.4f}")
